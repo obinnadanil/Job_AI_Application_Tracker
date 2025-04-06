347fb803c7e1dc0386fcf804ec4e21534868b6e1
Iloke Obinna <obinnadanil@gmail.com>
2025-04-07 00:33:25 +0100

    knexfile updated

--- knexfile.js
+++ knexfile.js
@@ -23,5 +23,22 @@ module.exports = {
       directory: './src/database/migrations'
     }
   },
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations'
+    }
+  },
   
 };
